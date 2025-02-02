81bb1643d1e4412e02a78972072080aca135478e
Dennis-89 <straub.dennis1@web.de>
2025-02-02 08:59:10 +0100

    Change API

--- bcs2/Scale.py
+++ bcs2/Scale.py
@@ -11,15 +11,15 @@ SCALE_ADDRESS = "5C:CA:D3:5E:65:8E"
 class Scale(DefaultDelegate):
     def __init__(self, scale_address):
         DefaultDelegate.__init__(self)
-        self.scanner = Scanner(0).withDelegate(self)
-        self.scanner.start()
-        self.scanner.clear()
-        self._first_seen = False
         self.scale_address = scale_address
+        self._scanner = Scanner(0).withDelegate(self)
+        self._scanner.start()
+        self._scanner.clear()
+        self._first_seen = False
         self._weight = 0
 
-    def measure(self, timeout):
-        self.scanner.scan(timeout)
+    def measure(self, timeout=1.0):
+        self._scanner.scan(timeout)
 
     @property
     def weight(self):
@@ -29,15 +29,13 @@ class Scale(DefaultDelegate):
         # `first_seen` is needed because the Scale send at first
         # a saved value.
         if is_new_data and device.addr == self.scale_address.lower():
-            if self._first_seen:
-                self._weight = (
-                    int.from_bytes(
-                        bytes.fromhex(device.getValueText(22)[-4:]), "little"
-                    )
-                    / 200
-                )
-            else:
+            if not self._first_seen:
                 self._first_seen = True
+                return
+            self._weight = (
+                int.from_bytes(bytes.fromhex(device.getValueText(22)[-4:]), "little")
+                / 200
+            )
 
 
 def main():
